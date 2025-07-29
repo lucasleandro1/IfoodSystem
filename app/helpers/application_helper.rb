9fbe75724b26b27c9e538877575f518cdb27e0ce
Lucas Leandro <lukkassoliveira2215@gmail.com>
2025-07-28 21:09:51 -0300

    create helper to status colors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,21 @@ module ApplicationHelper
     nome = email.split("@").first
     nome
   end
+
+  def status_color(status)
+    case status
+    when 'pendente'
+      '#ffc107'
+    when 'preparando'
+      '#fd7e14'
+    when 'em_rota'
+      '#0dcaf0'
+    when 'entregue'
+      '#198754'
+    when 'cancelado'
+      '#dc3545'
+    else
+      '#6c757d'
+    end
+  end
 end
